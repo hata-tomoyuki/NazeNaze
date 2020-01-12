bdc3636467c040cb7a5840ffa83e198b1f8df33a
hata-tomoyuki <h.tomo0602@gmail.com>
2020-01-12 09:30:31 +0900

    commit1

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  validates :text, presence: true
   belongs_to :theme
   belongs_to :user
 end
---
+++ app/views/posts/create.json.jbuilder
@@ -0,0 +1,2 @@
+json.text  @post.text
+json.date  @post.created_at.to_s(:datetime)
