488947623e2a70341f4b3a6bd3442b86bf5f9443
hata-tomoyuki <h.tomo0602@gmail.com>
2020-01-13 13:20:03 +0900

    try remote

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,17 @@
 class PostsController < ApplicationController
 
+  before_action :set_theme
+
   def create
     @post = Post.create(post_params)
-    if @post.save
-      respond_to do |format|
-        format.html { redirect_to theme_path(params[:theme_id]), notice: "メッセージを送信しました" }
-        format.json
-      end
-    end
+    # if @post.save
+    #   respond_to do |format|
+    #     format.html { redirect_to theme_path(params[:theme_id]), notice: "メッセージを送信しました" }
+    #     format.json
+    #   end
+    redirect_to theme_path(params[:theme_id])
+    # end
+    
   end
 
   def destroy
@@ -19,6 +23,10 @@ class PostsController < ApplicationController
   private
   def post_params
     params.require(:post).permit(:text).merge(theme_id: params[:theme_id], user_id: current_user.id)
+  end
+
+  def set_theme
+    @theme = Theme.find(params[:theme_id])  
   end
 
 end
--- app/views/posts/create.json.jbuilder
+++ app/views/posts/create.json.jbuilder
@@ -1,2 +1,2 @@
-json.text  @post.text
-json.date  @post.created_at.to_s(:datetime)
+# json.text  @post.text
+# json.date  @post.created_at.to_s(:datetime)
